08d33c0a8dc71d40f6fd8154ec3dbd8498bc4d3e
constantinneagu <neagu@geries.de>
2023-09-19 12:13:09 +0200

    added token authentication on unwrapping

--- tests/integration_tests/api/system_backend/test_wrapping.py
+++ tests/integration_tests/api/system_backend/test_wrapping.py
@@ -14,11 +14,15 @@ class TestWrapping(HvacIntegrationTestCase, TestCase):
             method_type=self.TEST_AUTH_METHOD_TYPE,
             path=self.TEST_AUTH_METHOD_PATH,
         )
-
-    def test_unwrap(self):
         self.client.write(
             path=f"auth/{self.TEST_AUTH_METHOD_PATH}/role/testrole",
         )
+
+    def tearDown(self):
+        self.client.sys.disable_auth_method(path=self.TEST_AUTH_METHOD_PATH)
+        return super().tearDown()
+
+    def test_unwrap(self):
         result = self.client.write(
             path="auth/{path}/role/testrole/secret-id".format(
                 path=self.TEST_AUTH_METHOD_PATH
@@ -31,3 +35,19 @@ class TestWrapping(HvacIntegrationTestCase, TestCase):
         logging.debug("unwrap_response: %s" % unwrap_response)
         self.assertIn(member="secret_id_accessor", container=unwrap_response["data"])
         self.assertIn(member="secret_id", container=unwrap_response["data"])
+
+    def test_unwrap_token_with_token_as_login(self):
+        result = self.client.write(
+            path="auth/{path}/role/testrole/secret-id".format(
+                path=self.TEST_AUTH_METHOD_PATH
+            ),
+            wrap_ttl="10s",
+        )
+        self.assertIn("token", result["wrap_info"])
+        old_token = self.client.token
+        self.client.logout()
+        unwrap_response = self.client.sys.unwrap(token=result["wrap_info"]["token"], use_token_to_authenticate=True)
+        logging.debug("unwrap_response: %s" % unwrap_response)
+        self.assertIn(member="secret_id_accessor", container=unwrap_response["data"])
+        self.assertIn(member="secret_id", container=unwrap_response["data"])
+        self.client.token = old_token
